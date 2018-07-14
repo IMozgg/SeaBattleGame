2c50c124e1be3f37fa376cf2e36590e6970f78d7
Mozgachev I.V <mozgachev.ivan@gmail.com>
2018-07-14 13:10:41 +0300

    Erase redundant scrabble public

--- Draw/IDrawing.java
+++ Draw/IDrawing.java
@@ -3,6 +3,6 @@ package SeaBattle.Draw;
 import SeaBattle.gamesInventory.Player;
 
 public interface IDrawing {
-    public void draw(Player player);
-    public void drawMask(Player p);
+    void draw(Player player);
+    void drawMask(Player p);
 }
