d4c83902b6f5f27c770145b49b8e2019be7e84a3
Mozgachev I.V <mozgachev.ivan@gmail.com>
2018-07-14 13:52:59 +0300

    Create GUI Form only

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package SeaBattle;
 
 import SeaBattle.Draw.DrawingToDisplay;
+import SeaBattle.Draw.Form;
 import SeaBattle.Draw.IDrawing;
 import SeaBattle.InputData.IInputData;
 import SeaBattle.InputData.InputKey;
@@ -8,8 +9,10 @@ import SeaBattle.InputData.InputKey;
 public class Main {
     public static void main(String[] args) {
         IDrawing drawing = new DrawingToDisplay();
-        IInputData inputData = new InputKey();
+        Form form = new Form("v1.0");
+        form.intForm();
+        /*IInputData inputData = new InputKey();
         Game game = Game.getInstance(drawing, inputData);
-        System.out.println("Выиграл игрок: " + game.startAutoGame());
+        System.out.println("Выиграл игрок: " + game.startAutoGame());*/
     }
 }
